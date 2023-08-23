6044876cdf12b668d143d17c9661ae6135b5c528
Luiz <valuiz1@gmail.com>
2023-08-23 17:55:13 -0300

    Added class handling for parser

--- libraries/source_parser.py
+++ libraries/source_parser.py
@@ -13,23 +13,32 @@ class Parser:
         callables = re.findall(pattern, self.source)
         return [callable[:-1] for callable in callables]
 
+    def __get_call_names_from_node(self, node):
+        internal_source = "".join([astunparse.unparse(node) for node in node.body])
+        unparsed_calls = re.findall(r"[a-zA-Z0-9]+\(", internal_source)
+        parsed_calls = [calls[:-1] for calls in unparsed_calls]
+        return parsed_calls
+
     def encapsulated_callables(self) -> dict:
-        tree = ast.parse(source)
+        tree = ast.parse(self.source)
         internal_calls = {}
         for node in ast.iter_child_nodes(tree):
             # Right now only identifies functions
             if isinstance(node, ast.FunctionDef):
-                print(node.name)
-                internal_source = "".join([astunparse.unparse(node) for node in node.body])
-                unparsed_calls = re.findall(r"[a-zA-Z0-9]+\(", internal_source)
-                parsed_calls = [calls[:-1] for calls in unparsed_calls]
-                print(parsed_calls)
+                parsed_calls = self.__get_call_names_from_node(node)
                 internal_calls[node.name] = parsed_calls
+            elif isinstance(node, ast.ClassDef):
+                class_name = node.name
+                for int_node in node.body:
+                    if isinstance(int_node, ast.FunctionDef):
+                        parsed_calls = self.__get_call_names_from_node(int_node)
+                        method_name = int_node.name
+                        internal_calls[f"{class_name}.{method_name}"] = parsed_calls
         return internal_calls
 
 
 if __name__ == "__main__":
-    with open("libraries/mapper.py") as file:
+    with open("libraries/module.py") as file:
         source = file.read()
         p = Parser(source)
         print(p.encapsulated_callables())
