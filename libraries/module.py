ccd8343163f2356520274d65d1c63849a5f600b3
Luiz <valuiz1@gmail.com>
2023-09-05 17:27:22 -0300

    New test module

--- libraries/module.py
+++ libraries/module.py
@@ -1,37 +1,52 @@
 from pprint import pprint
-
-
-class Cls:
-    def __init__(self) -> None:
-        func_x()
-        func_y()
-
-    def method(self):
-        call1()
-        x = call2()
-
-
-def func_x():
-    func_y()
+from RPA.Browser.Selenium import Selenium
+import os
 
 
 def func_y():
     pprint("a")
 
 
+class Cls:
+    """This is Cls documentation"""
+
+    func_y()
+    aaa = 20
+
+    def __init__(self) -> None:
+        self.aaa = 40
+        x = func_x()
+        func_y()
+
+    def method(self):
+        call2()
+
+
+def func_x(a: str = 0, b: str = 1):
+    """
+    This is a docstring
+    """
+    var = func_y()
+
+
 def function():
-    call1()
+    print("b")
     call2()
 
 
 def call1():
-    pass
+    b = Selenium()
+    b.open_available_browser()
+    b.set_window_size(1920, 1600)
+    b.maximize_browser_window()
+    x = Cls()
+    x.method()
 
 
 def call2():
+    """This is call2 documentation"""
     pass
 
 
-function()
-
-{function: {call1: {}, call2: {}}}
+if __name__ == "__main__":
+    function()
